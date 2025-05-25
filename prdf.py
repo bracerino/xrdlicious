ff0919d9d0bf87417240de69f65897f8dddb63cd
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-05-25 09:36:12 +0300

    Update prdf.py

--- prdf.py
+++ prdf.py
@@ -365,12 +365,36 @@ if show_database_search:
 
             if not db_choices:
                 st.warning("Please select at least one database to search.")
-
+        ELEMENTS = [
+                    'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
+                    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca',
+                    'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn',
+                    'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr',
+                    'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn',
+                    'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd',
+                    'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb',
+                    'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg',
+                    'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th',
+                    'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm',
+                    'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds',
+                    'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og'
+                ]
         with cols2:
-            search_query = st.text_input(
-                "Enter elements separated by spaces (e.g., Sr Ti O):",
-                value="Sr Ti O"
+            selected_elements = st.multiselect(
+                "Select elements for search:",
+                options=ELEMENTS,
+                default=["Sr", "Ti", "O"],
+                help="Choose one or more chemical elements"
             )
+            search_query = " ".join(selected_elements) if selected_elements else ""
+    
+    
+    
+       # with cols2:
+       #     search_query = st.text_input(
+       #         "Enter elements separated by spaces (e.g., Sr Ti O):",
+       #         value="Sr Ti O"
+       #     )
 
         if st.button("Search Selected Databases"):
             if not db_choices:
