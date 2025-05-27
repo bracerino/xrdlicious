3ef859b57781bd5d664c01fb916c853b42fc02a4
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-05-27 17:22:00 +0300

    Update prdf.py

--- prdf.py
+++ prdf.py
@@ -67,9 +67,9 @@ mem_info = process.memory_info()
 memory_usage = mem_info.rss / (1024 ** 2)  # in MB
 
 # Check if memory exceeds 1600 MB
-if memory_usage > 600:
+if memory_usage > 450:
    # Show warning message
-   st.warning(f"⚠️ **Memory Warning!** Current usage: {memory_usage:.2f} MB exceeds 1600 MB limit. Sorry, we are using available free resources. :[ In 10 seconds, there will be a forced rerun with cleared memory. If you wish to run calculations on extensive data, please compile this application locally. Cleaning cache and restarting in 10 seconds...")
+   st.markdown(f"# ⚠️ **Memory Warning!** Current usage: {memory_usage:.2f} MB exceeds 1600 MB limit. Sorry, we are using available free resources. :[ In 10 seconds, there will be a forced rerun with cleared memory. If you wish to run calculations on extensive data, please compile this application locally. Cleaning cache and restarting in 10 seconds...")
    
    # Wait 10 seconds
    time.sleep(10)
