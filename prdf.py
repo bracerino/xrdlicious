925f58eebf02e589b1fb91299cc199c891dacb2c
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-05-28 12:58:59 +0300

    Update prdf.py

--- prdf.py
+++ prdf.py
@@ -259,7 +259,7 @@ calc_mode = st.sidebar.multiselect(
     default=["🔬 Structure Modification","💥 Powder Diffraction" ]
 )
 
-if "📉 PRDF from LAMMPS/XYZ trajectories" in calc_mode
+if "📉 PRDF from LAMMPS/XYZ trajectories" in calc_mode:
         st.subheader("This module calculates the Pair Radial Distribution Function (PRDF) across frames in LAMMPS or XYZ trajectories. Due to its high computational demands, it cannot be run on our free online server. Instead, it is provided as a standalone module that must be compiled and executed locally. Please visit to see how to compile and run the code:")
     st.markdown(
         '<p style="font-size:24px;">🔗 <a href="https://github.com/bracerino/PRDF-CP2K-LAMMPS" target="_blank">Downlaod the PRDF calculator for LAMMPS/XYZ trajectories</a></p>',
