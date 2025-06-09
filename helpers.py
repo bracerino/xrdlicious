d170dcb6427263e2f3ad6441a590110dbcfb3407
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-06-10 00:27:27 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1261,7 +1261,7 @@ def show_xrdlicious_roadmap():
 -------------------------------------------------------------------------------------------------------------------
 
 #### Code optimization 
-* ⏳ Optimizing the code for better performance. ✅ Optimized search in COD database.
+* ⏳ Optimizing the code for better performance. ⏳Separate critical parameters (such as wavelength, new file, debye-waller factors) for diffraction patterns complete recalculations from non-critical (such as intensity scale or x-axis units) ✅ Optimized search in COD database.
 
 #### Wavelength Input: Energy Specification
 * ⏳ Allow direct input of X-ray energy (keV) for synchrotron measurements, converting to wavelength automatically.
