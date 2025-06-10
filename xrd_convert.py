4d8b15356916a085d313cdb1501ef260453c3b5a
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-06-10 17:46:41 +0300

    Update xrd_convert.py

--- xrd_convert.py
+++ xrd_convert.py
@@ -1412,7 +1412,7 @@ def run_data_converter():
     #)
     st.info(
         "📄🔁📄 Upload one or more data powder diffraction files to convert them to a different format. .**xy ➡️ .xrdml, .ras, .raw**. "
-        "Or **.xrdml, .ras, .raw ➡️ .xy**. \n\n ⚠️ an older **.raw** format can currently produce incorrect x-axis values. "
+        "Or **.xrdml, .ras, .raw ➡️ .xy**. \n\n ⚠️ Older **.raw** format can currently produce incorrect x-axis values. "
         "Check if they are correct in the converted .xy format.")
 
     allow_batch = st.checkbox(
