06fb44986775de3ef73a3d2e7cbbcdca764c137a
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-06-11 12:38:27 +0300

    Update prdf.py

--- prdf.py
+++ prdf.py
@@ -120,7 +120,7 @@ col1, col2 = st.columns([0.8, 0.4])
 with col2:
     st.info(
         "🌀 Developed by **[IMPLANT team](https://implant.fs.cvut.cz/)**. **[Quick tutorial here](https://youtu.be/jHdaNVB2UWE)**. Spot a bug or have a feature idea? Let us know at: "
-        "lebedmi2@cvut.cz. To compile this application locally, please visit our **[GitHub page](https://github.com/bracerino/xrdlicious)** and find the tutorial there."
+        "**lebedmi2@cvut.cz**. To compile this application locally, please visit our **[GitHub page](https://github.com/bracerino/xrdlicious)** and find the tutorial there."
     )
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
