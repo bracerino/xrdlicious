d2eadcaed5d261f68bfc1592192f31f8071b3e80
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-12-04 16:27:12 +0100

    Update prdf.py

--- prdf.py
+++ prdf.py
@@ -116,7 +116,7 @@ with col3:
 with col2:
     st.info(
         "🌀 Developed by **[IMPLANT team](https://implant.fs.cvut.cz/)**. Spot a bug or have a feature idea? Let us know at: "
-        "**lebedmi2@cvut.cz**. To compile the app locally, visit our **[GitHub page](https://github.com/bracerino/xrdlicious)**. If you like the app, please cite **[article in IUCr](https://journals.iucr.org/j/issues/2025/05/00/hat5006/index.html).** "
+        "**lebedmi2@cvut.cz**. To compile the app locally, visit our **[GitHub page](https://github.com/bracerino/xrdlicious)**. If you like the app, please cite **[article in IUCr](https://journals.iucr.org/j/issues/2025/05/00/hat5006/index.html). ❤️🫶 **[Donations always appreciated!](https://buymeacoffee.com/bracerino)**"** "
     )
 
 # with col3:
